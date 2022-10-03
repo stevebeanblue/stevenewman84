8a308378f06097785be47e3d504ad3939a91f3b2
stevebeanblue <stevebeanblue@aol.com>
2022-10-03 11:24:55 +0100

    update to css mime type on settings

--- stevenewman84/settings.py
+++ stevenewman84/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     BASE_DIR.joinpath('static')
 ]
 
-STATIC_ROOT = '/var/www/stevebeanblue/static'
+STATIC_ROOT = '/var/www/stevenewman84/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media/')
